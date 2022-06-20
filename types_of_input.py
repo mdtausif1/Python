333f2bc328dc7b9116a4f8b5043ff095669fbe3a
mdtausif1 <97949757+mdtausif1@users.noreply.github.com>
2022-06-20 21:49:08 +0530

    Add files via upload

---
+++ types_of_input.py
@@ -0,0 +1,13 @@
+x = int(input('Enter the value for x'))
+y = int(input('Enter the value for y'))
+z = x+y
+print(z)
+
+evalute = eval(input('Enter the Values with sign to evaluate'))
+print(evalute)
+
+A = input('Enter A Char')
+B = input('Enter B char')
+c= A+B
+print(c)
+
